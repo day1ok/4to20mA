942a7ac9d95e59f81ab03c298426ed1518e74b05
day1ok <day1ok@gmail.com>
2016-05-27 11:09:32 -0700

    Add files via upload

---
+++ _4_20maCode.ino
@@ -0,0 +1,17 @@
+int analogPin = 0;     // potentiometer wiper (middle terminal) connected to analog pin 3
+                       // outside leads to ground and +5V
+int val = 0;           // variable to store the value read
+float Temp = 0;
+
+void setup()
+{
+  Serial.begin(9600);          //  setup serial
+}
+
+void loop()
+{
+  val = analogRead(analogPin);    // read the input pin
+  Temp = (float) ((((val+68)-4)/16*(1023-222) + 222)*.000911);
+  Serial.println(Temp);             // debug value
+  delay(500);
+}
